0b343e3bdc3d2dff60d18febce376ebd7a515671
Anastasiia Kutsenko <145927427+mercury-paws@users.noreply.github.com>
2025-02-24 15:47:18 +0100

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-axios.defaults.baseUrl = "https://connections-api.herokuapp.com/";
+// axios.defaults.baseUrl = "https://connections-api.herokuapp.com/";
+axios.defaults.baseURL = "https://cors-anywhere.herokuapp.com/https://connections-api.herokuapp.com/";
+
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
