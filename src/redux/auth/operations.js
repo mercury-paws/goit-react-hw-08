4cb6a3c86b7d0ed3e6a36eba9b422fc8c1485ed9
Anastasiia Kutsenko <145927427+mercury-paws@users.noreply.github.com>
2025-02-28 09:35:07 +0100

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-axios.defaults.baseUrl = "https://connections-api.herokuapp.com/";
+
+axios.defaults.baseUrl = "https://connections-api.goit.global/";
+// axios.defaults.baseUrl = "https://connections-api.herokuapp.com/";
 // axios.defaults.baseURL = "https://cors-anywhere.herokuapp.com/https://connections-api.herokuapp.com/";
 
 
